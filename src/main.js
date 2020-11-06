b02f1cad471491d1ece54150f6531e8c086bdd54
Prabu Devan S <PrabuDevanDX@gmail.com>
2020-11-06 20:06:39 +0530

    added access token

--- src/main.js
+++ src/main.js
@@ -12,11 +12,13 @@ if (!window.gapi) {
     gapi.load('client:auth2', () => {
       gapi.client.init({
         apiKey: 'AIzaSyCZpskkk3ggfa7TRxf4kFC5BcvPaueMAGY',
-        clientId: '447243147492-4nm0frk4cbr2r0dk4da6esoltkp89ar0.apps.googleusercontent.com',
+        clientId: '447243147492-bms9maufkqeu54e7sbtf9ahlmvjnskuj.apps.googleusercontent.com',
         scope: 'https://www.googleapis.com/auth/drive.metadata.readonly',
         discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"]
       }).then(() => {
         store.state.googleAuth = gapi.auth2.getAuthInstance();
+        store.state.accessToken = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().access_token;
+        console.log(store.state.accessToken);
         if (store.state.googleAuth.isSignedIn.get()) {
           gapi.client.drive.files.list({
             pageSize: 10,
